6008720428c5863fac48cce5f4efcac9631af2ef
Art <bobrolevv@yandex.ru>
2021-09-03 13:18:17 +0700

    tl

--- summa.py
+++ summa.py
@@ -91,23 +91,35 @@
 #         # break
 
 # Ближайший ноль ======================
+def find_null(input_list, start):
+    i1 = 0
+    res1 = None
+    res2 = None
+    len_list = len(input_list)
+    for x in range(start, len_list):
+        lambda res1, x, input_list[x]: res1 = i1 if input_list[x] == 0
+
+        # if input_list[x] == 0:
+        #     res1 = i1
+        #     break
+        i1 += 1
+
+    i1 = 0
+    for y in range(start, -1, -1):
+        if input_list[y] == 0:
+            res2 = i1
+            break
+        i1 += 1
+
+    try:
+        return min(res1, res2)
+    except:
+        if res1 is None: return res2
+        if res2 is None: return res1
+
 street = input()
-hoom = list(map(int, input('Введите через пробел ').split()))
-hoom_num = list(enumerate(hoom, start=1))
-hoom_null = list(filter(lambda x: x[1] == 0, hoom_num))
-# hoom_min = min(hoom_num, key=lambda i : i[1])
-print(hoom_num)
-# print(hoom_min)
+hoom = list(map(int, input().split()))
 
-print(list(filter(lambda x: x[1] == 0, hoom_num)))
+l = " ".join([str(find_null(hoom, x)) for x in range(0, len(hoom))])
 
-def find_null(input_list):#, start):
-    i = 0
-    for x in input_list:
-        if x[1] != 0:
-            i += 1
-    return i
-
-print(find_null(hoom_num))
-
-
+print(l)
