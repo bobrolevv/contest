6f3a1882aaf122a8d0c08ce60fad24f4ffa0b056
Art <bobrolevv@yandex.ru>
2021-09-14 08:32:06 +0700

    c

--- spr_13/code.py
+++ spr_13/code.py
@@ -1,8 +1,8 @@
 # Comment it before submitting
-class Node:
-    def __init__(self, value, next_item=None):
-        self.value = value
-        self.next_item = next_item
+# class Node:
+#     def __init__(self, value, next_item=None):
+#         self.value = value
+#         self.next_item = next_item
 
 
 def solution(node, idx):
@@ -14,24 +14,19 @@ def solution(node, idx):
         return node
 
     if idx == 0:
-        node.next_item = None
-        # node.value = None
-        return node
+        return node.next_item
 
     previous_node = get_node_by_index(node, idx - 1)
     previous_node.next_item = previous_node.next_item.next_item
 
-    while node:
-        print(node.value, end='\n')
-        node = node.next_item
     return node
 
-def test():
-    node3 = Node("node3", None)
-    node2 = Node("node2", node3)
-    node1 = Node("node1", node2)
-    node0 = Node("node0", node1)
-    new_head = solution(node0, 1)
-    # result is node0 -> node2 -> node3
-
+# def test():
+#     node3 = Node("node3", None)
+#     node2 = Node("node2", node3)
+#     node1 = Node("node1", node2)
+#     node0 = Node("node0", node1)
+#     new_head = solution(node0, 1)
+#     # result is node0 -> node2 -> node3
+#
 # test()
