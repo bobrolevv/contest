e5cd54d599593028d2cdd7129b375aa5fee3ffd6
Art_book <bobrolevv@yandex.ru>
2021-09-20 19:32:29 +0700

    a - rew 1

--- spr_13/final_A.py
+++ spr_13/final_A.py
@@ -1,4 +1,16 @@
 # id 53206892
+class DequeException(Exception):
+    pass
+
+
+class EmptyDeque(DequeException):
+    pass
+
+
+class FullDeque(DequeException):
+    pass
+
+
 class Deque:
     def __init__(self, n):
         self.items: list[any] = [None] * n
@@ -16,35 +28,43 @@ class Deque:
             self.items[self.tail] = item
             self.size += 1
         else:
-            print('error')
+            raise FullDeque('stack is full')
+            # print('error')
 
-    def push_front(self, x) -> object:
+    def push_front(self, x) -> list:
         if self.size != self.maximum:
             self.items[self.head] = x
             self.head = (self.head + 1) % self.maximum
             self.size += 1
         else:
-            print('error')
+            raise FullDeque('stack is full')
+            # print('error')
 
     def pop_front(self) -> object:
         if self.is_empty():
-            return 'error'
-        x = self.items[self.head - 1]
-        self.items[self.head - 1] = None
+            raise EmptyDeque('stack is empty')
+            # return 'error'
+        # x = self.items[self.head - 1
+        # self.items[self.head - 1] = None
+        # self.head = (self.head - 1) % self.maximum
         self.head = (self.head - 1) % self.maximum
+        x = self.items[self.head]
+        # self.items[self.head - 1] = None
+
         self.size -= 1
         return x
 
     def pop_back(self) -> object:
         if self.is_empty():
-            return 'error'
+            raise EmptyDeque('stack is empty')
         x = self.items[self.tail]
         self.items[self.tail] = None
         self.tail = (self.tail + 1) % self.maximum
         self.size -= 1
         return x
 
-def deque_maker(input_data):
+def deque_maker(input_data:str) -> list:
+    result = []
     # использую чтение из файла, т.к. с input() не проходит TL 23 тесте
     with open(input_data) as input_data:
         command_count: int = int(input_data.readline())
@@ -52,10 +72,17 @@ def deque_maker(input_data):
 
         for i in range(0, command_count):
             comm_list = input_data.readline().split()
-            if comm_list[0] == 'push_back': deque.push_back(comm_list[1])
-            elif comm_list[0] == 'pop_back': print(deque.pop_back())
-            elif comm_list[0] == 'push_front': deque.push_front(comm_list[1])
-            elif comm_list[0] == 'pop_front': print(deque.pop_front())
+            try:
+                if comm_list[0] == 'push_back': deque.push_back(comm_list[1])
+                elif comm_list[0] == 'pop_back': print(deque.pop_back())
+                elif comm_list[0] == 'push_front': deque.push_front(comm_list[1])
+                elif comm_list[0] == 'pop_front': print(deque.pop_front())
+            except EmptyDeque:
+                print('error')
+            except FullDeque:
+                pass
+
+    return result
 
 if __name__ == '__main__':
 
