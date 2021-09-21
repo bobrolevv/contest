f45aafa44de3ef1059b41eae23e0e1ca885bcb70
Art_book <bobrolevv@yandex.ru>
2021-09-21 22:32:18 +0700

    a - rew 1 DONE

--- spr_13/final_A.py
+++ spr_13/final_A.py
@@ -1,4 +1,4 @@
-# id 53206892
+# id 53323246
 class DequeException(Exception):
     pass
 
@@ -27,41 +27,39 @@ class Deque:
             self.tail = (self.tail - 1) % self.maximum
             self.items[self.tail] = item
             self.size += 1
+
         else:
             raise FullDeque('stack is full')
-            # print('error')
 
-    def push_front(self, x) -> list:
+    def push_front(self, item) -> list:
         if self.size != self.maximum:
-            self.items[self.head] = x
+            self.items[self.head] = item
             self.head = (self.head + 1) % self.maximum
             self.size += 1
         else:
             raise FullDeque('stack is full')
-            # print('error')
 
-    def pop_front(self) -> object:
+    def pop_front(self) -> list:
         if self.is_empty():
             raise EmptyDeque('stack is empty')
-            # return 'error'
-        # x = self.items[self.head - 1
-        # self.items[self.head - 1] = None
-        # self.head = (self.head - 1) % self.maximum
+
         self.head = (self.head - 1) % self.maximum
-        x = self.items[self.head]
-        # self.items[self.head - 1] = None
+        item = self.items[self.head]
 
         self.size -= 1
-        return x
+        return item
 
-    def pop_back(self) -> object:
+    def pop_back(self) -> list:
         if self.is_empty():
             raise EmptyDeque('stack is empty')
-        x = self.items[self.tail]
-        self.items[self.tail] = None
+
+        item = self.items[self.tail]
         self.tail = (self.tail + 1) % self.maximum
         self.size -= 1
-        return x
+        return item
+
+    def __str__(self):
+        return '-'.join(str(x) for x in self.items)
 
 def deque_maker(input_data:str) -> list:
     result = []
@@ -71,20 +69,30 @@ def deque_maker(input_data:str) -> list:
         deque: Deque = Deque(int(input_data.readline()))
 
         for i in range(0, command_count):
-            comm_list = input_data.readline().split()
-            try:
-                if comm_list[0] == 'push_back': deque.push_back(comm_list[1])
-                elif comm_list[0] == 'pop_back': print(deque.pop_back())
-                elif comm_list[0] == 'push_front': deque.push_front(comm_list[1])
-                elif comm_list[0] == 'pop_front': print(deque.pop_front())
-            except EmptyDeque:
-                print('error')
-            except FullDeque:
-                pass
+            cmnd, *prms = input_data.readline().split()
 
+            try:
+                # getattr(deque, cmnd(*prms)) # - очень локанично, но у меня
+                # вызывает ошибку: TypeError: 'str' object is not callable
+                # --------------------
+                # пробовал так:
+                # getattr(deque, f'{cmnd}({*prm})')  # так на синтаксис руг-ся
+                #                          ^
+                # SyntaxError: can't use starred expression here
+
+                if cmnd == 'push_back': deque.push_back(*prms)
+                elif cmnd == 'pop_back': result.append(deque.pop_back())
+                elif cmnd == 'push_front': deque.push_front(*prms)
+                else: result.append(deque.pop_front())
+            except EmptyDeque:
+                result.append('error')
+            except FullDeque:
+                result.append('error')
     return result
 
 if __name__ == '__main__':
 
-    input_data = 'input_A.txt'
-    deque_maker(input_data)
+    input_data = 'input.txt'
+    result = deque_maker(input_data)
+    for i in result:
+        print(i)
