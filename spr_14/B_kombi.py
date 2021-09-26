b15841791fa9aff6cb1d9547b4d7be4f2bf6ab38
Artem <bobrolevv@yandex.ru>
2021-09-26 23:57:15 +0700

    spr_14 B-done

--- spr_14/B_kombi.py
+++ spr_14/B_kombi.py
@@ -1,4 +1,4 @@
-DICT_1 = {
+NO_KIA = {
     2:'abc',
     3:'def',
     4:'ghi',
@@ -8,70 +8,20 @@ DICT_1 = {
     8:'tuv',
     9:'wxyz'
 }
-# нужны три параметра:
-# набор кнопок, которые будем обрабатывать
-# текущая строка букв
-# массив с результатами
 
-def combi(data: str,
-          strg: str,
-          counter: int,
-          ans: str):
 
+def combi(data: list, counter: int, n:int, ans: list):
     if counter == len(data):
-        return ans
-    else:
+        print(ans, end=' ')
+        return
+    if counter < len(data):
+        for i in data[n]:
+            combi(data, counter + 1, n+1, ans + i)
 
-        combi(data, strg, counter+1, ans + data[]):
+inp = input()
+data = []
+for i in range(0, len(inp)):
+    d = int(inp[i])
+    data.append(NO_KIA[d])
 
-
-
-
-
-
-# ===============
-# def combi(data, prefix='', n=0, result=None):
-#     result = [] if result is None else result
-#     if len(prefix) == 2:
-#         result.append(prefix)
-#     else:
-#         for i in data[n]:
-#             combi(data, prefix+i, n+1, result)
-#         combi(data, prefix, n+1, result)
-#     return result
-# # ===============
-#
-# # def combi(data: list,
-# #           variants:list,
-# #           counter: int,
-# #           x: int,
-# #           y: int,
-# #           ans: str):
-# #
-# #     if counter == variants[0]:
-# #         print(ans)
-# #         return
-# #     if counter < variants[0]:
-# #         # n = len(data[0])
-# #         combi(data,
-# #               variants,
-# #               counter + 1,
-# #               # if x < 3: x+=1 else x=0,
-# #               x-1 if x!=0 else variants[1],
-# #               y-1 if y!=len(data[1]) else 0,
-# #               ans + data[variants[1] - x][y-1] + ' '
-# #         )
-#
-# inp = input()
-# data = []
-# variants = [1, 1]
-# for i in range(0, len(inp)):
-#     d = int(inp[i])
-#     data.append(DICT_1[d])
-#     variants[0] = variants[0] * len(DICT_1[d])
-#
-# variants[1] = len(data)
-# # print(data, variants)
-#
-# # combi(data, variants, 0, variants[1], len(data[0]), '')
-# combi(data)
+combi(data, 0, 0, '')
